ea972854b9bfc72d2196a648cdc35ead35737190
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-09-19 21:00:29 -0500

    Use GNews API

--- assets/javascript/gnews.js
+++ assets/javascript/gnews.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
   $("#name-image").hide();
 })
 
+// 83f5e784408070fb58bc521846521f8c
+// example&token=API-Token
 
 function displayArticles(celebrity) {
 
-  var queryUrl = "https://newsapi.org/v2/everything?q=" + celebrity + "&apiKey=f9a29869e5cd42f4b0d2a6a4b9a85c6c";
+  var queryUrl = `https://gnews.io/api/v4/search?q=${celebrity}&token=83f5e784408070fb58bc521846521f8c`;
 
 
     $.ajax({
@@ -21,11 +23,12 @@ function displayArticles(celebrity) {
       var articleUrl1 = response.articles[0].url;
       var articleLink1 = $('<a href="' + articleUrl1 + '" target="_blank">' + title1 + '</a>');
       var articleHead1 = $("<h2>");
+
       articleHead1.addClass("articleHead");
       $(articleHead1).append(articleLink1);
       $("#article1").append(articleHead1);
 
-      var imgUrl1 = response.articles[0].urlToImage;
+      var imgUrl1 = response.articles[0].image;
       var articleImg1 = $('<img src="' + imgUrl1 + '">');
       articleImg1.addClass("articleImg");
       $("#article1").append(articleImg1);
@@ -40,7 +43,7 @@ function displayArticles(celebrity) {
       $(articleHead2).append(articleLink2);
       $("#article2").append(articleHead2);
 
-      var imgUrl2 = response.articles[1].urlToImage;
+      var imgUrl2 = response.articles[1].image;
       var articleImg2 = $('<img src="' + imgUrl2 + '">');
       articleImg2.addClass("articleImg");
       $("#article2").append(articleImg2);
@@ -55,7 +58,7 @@ function displayArticles(celebrity) {
       $(articleHead3).append(articleLink3);
       $("#article3").append(articleHead3);
 
-      var imgUrl3 = response.articles[2].urlToImage;
+      var imgUrl3 = response.articles[2].image;
       var articleImg3 = $('<img src="' + imgUrl3 + '">');
       articleImg3.addClass("articleImg");
       $("#article3").append(articleImg3);
